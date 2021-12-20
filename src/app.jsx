eeca1b1485d7446a84b3b8308514b87f47f8c98d
ohk9412 <ohk9412@naver.com>
2021-12-20 16:42:55 +0900

    show video list with title

---
+++ src/app.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import './app.css';
+import VideoList from './components/video_list/video_list';
+
+function App() {
+  const [videos, setVideos] = useState([]);
+
+
+  useEffect(()=> {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=MostPopular&maxResults=25&key=AIzaSyA88qxQw-WiQi0F23iUXwDHlbh9ugiliRQ", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
+
+  return <VideoList videos={videos} />;
+    
+}
+
+export default App;
