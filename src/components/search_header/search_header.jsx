5010b3fe4a0113e75f346eee671bfad5dfd997dc
ohk9412 <ohk9412@naver.com>
2021-12-27 13:12:29 +0900

    add youtube service

---
+++ src/components/search_header/search_header.jsx
@@ -0,0 +1,38 @@
+import styles from './search_header.module.css'
+import React, { useRef } from 'react';
+
+const SearchHeader = ({onSearch}) => {
+    const inputRef = useRef();
+    const handleSearch = () => {
+        const value = inputRef.current.value;
+        console.log(value);
+        onSearch(value);
+    };
+    const onClick = () => {
+        handleSearch();
+    };
+    const onKeypress = (event) => {
+        if(event.key==='Enter'){
+            handleSearch();
+        }
+    };
+    return (
+        <header className={styles.header}>
+            <div className={styles.logo}>
+                <img className={styles.img} src='/images/logo.png' alt='logo'/>
+                <h1 className={styles.title} >Youtube</h1>
+            </div>
+            <input 
+            ref={inputRef}
+            className={styles.input}
+             type="search" 
+             placeholder='search...'
+              onKeyPress={onKeypress}/>
+            <button className={styles.button} type='submit' onClick={onClick}>
+                <img className={styles.buttonImg} src='/images/search.png' alt='search' />
+            </button>
+        </header>
+    )
+}
+
+export default SearchHeader;
