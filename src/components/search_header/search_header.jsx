0dbe4028977cbc20286c76e2c705cc861c6e3b4f
ohk9412 <ohk9412@naver.com>
2022-05-23 20:58:30 +0900

    build test

--- src/components/search_header/search_header.jsx
+++ src/components/search_header/search_header.jsx
@@ -1,5 +1,6 @@
 import styles from './search_header.module.css'
 import React, { memo, useRef } from 'react';
+// import logo from '/images/logo.png/'
 
 const SearchHeader = memo(({onSearch}) => {
     const inputRef = useRef();
@@ -19,7 +20,7 @@ const SearchHeader = memo(({onSearch}) => {
     return (
         <header className={styles.header}>
             <div className={styles.logo}>
-                <img className={styles.img} src='/images/logo.png' alt='logo'/>
+                <img className={styles.img} src={process.env.PUBLIC_URL + '/images/logo.png'} alt='logo'/>
                 <h1 className={styles.title} >Youtube</h1>
             </div>
             <input 
@@ -29,7 +30,7 @@ const SearchHeader = memo(({onSearch}) => {
              placeholder='search...'
               onKeyPress={onKeypress}/>
             <button className={styles.button} type='submit' onClick={onClick}>
-                <img className={styles.buttonImg} src='/images/search.png' alt='search' />
+                <img className={styles.buttonImg} src={process.env.PUBLIC_URL + '/images/search.png'} alt='search' />
             </button>
         </header>
     );
